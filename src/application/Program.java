733f085f7cd2c15d5348067f0052d20b5ce8d6e7
Tales  Monteiro <=>
2020-11-18 11:28:20 -0300

    First Class -> position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hellow World");
+		Position position = new Position(1,2);
+		
+		System.out.println(position);
 
 	}
 
